422c3aee85decdf18e1b494c2b789bc4ed43722a
joseph deutsch <jld@joseph-laptop.fios-router.home>
2018-02-01 15:37:05 -0500

    added emacs themes and iterm settings

---
+++ emacs.d/init.el
@@ -0,0 +1,24 @@
+;; -*- mode: elisp -*-
+(setq inhibit-splash-screen t) ; disable splash screen
+
+; loading org
+(require 'org)
+(define-key global-map "\C-cl" 'org-store-link)
+(define-key global-map "\C-ca" 'org-agenda)
+(setq org-log-done t)
+
+
+; loading themes
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes/")
+
+
+;; -*- visual line wrap setting -*-
+
+; in Visual Line mode C-a, C-e, and C-k only apply to the visual lines
+; this purports to return to their original behavior but seems to get ignored
+; (setq line-move-visual nil)
+
+(global-visual-line-mode t) ;always use word wrap
+
+; this shows where word wraps are so i can tell them from actual newlines
+(setq visual-line-fringe-indicators '(left-curly-arrow right-curly-arrow))
---
+++ emacs.d/themes/tomorrow-night-bright-theme.el
@@ -0,0 +1,4 @@
+(require 'color-theme-tomorrow
+         (locate-file "color-theme-tomorrow.el" custom-theme-load-path))
+
+(color-theme-tomorrow--define-theme night-bright)
